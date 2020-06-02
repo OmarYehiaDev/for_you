86cd5a1910b8c667f6275f5bd4559fcbbd49f18f
Omar Yehia <omaryahiatawfiek@gmail.com>
2020-06-02 21:25:08 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'H.B.D to you',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final PageController pageController = PageController();
+
+  final List<GalleryExampleItem> galleryItems = <GalleryExampleItem>[
+    GalleryExampleItem(
+      id: "tag1",
+      imageUrl: "assets/gallery1.jpg",
+    ),
+    GalleryExampleItem(
+      id: "tag3",
+      imageUrl: "assets/gallery2.jpg",
+    ),
+    GalleryExampleItem(
+      id: "tag4",
+      imageUrl: "assets/gallery3.jpg",
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("H.B.D to you"),
+      ),
+      body: Container(
+        child: PhotoViewGallery.builder(
+          scrollDirection: Axis.vertical,
+          scrollPhysics: const BouncingScrollPhysics(),
+          builder: (BuildContext context, int index) {
+            return PhotoViewGalleryPageOptions(
+              imageProvider: AssetImage(
+                galleryItems[index].imageUrl,
+              ),
+              initialScale: PhotoViewComputedScale.contained * 0.8,
+              heroAttributes: PhotoViewHeroAttributes(
+                tag: galleryItems[index].id,
+              ),
+            );
+          },
+          itemCount: galleryItems.length,
+          loadingBuilder: (context, event) => Center(
+            child: Container(
+              width: 20.0,
+              height: 20.0,
+              child: CircularProgressIndicator(
+                value: event == null
+                    ? 0
+                    : event.cumulativeBytesLoaded / event.expectedTotalBytes,
+              ),
+            ),
+          ),
+          pageController: pageController,
+        ),
+      ),
+    );
+  }
+}
+
+class GalleryExampleItem {
+  GalleryExampleItem({this.id, this.imageUrl});
+
+  final String id;
+  final String imageUrl;
+}
